24c508335526094ea53be6a1ae444cb22c677749
William Roofe <willroofe@proton.me>
2026-04-03 18:37:59 +0100

    feat: add TabIndex and IsActiveTab to PanelSlotConfig

--- Volt/UI/Panels/PanelSlotConfig.cs
+++ Volt/UI/Panels/PanelSlotConfig.cs
@@ -6,4 +6,6 @@ public class PanelSlotConfig
     public PanelPlacement Placement { get; set; } = PanelPlacement.Left;
     public double Size { get; set; } = 250;
     public bool Visible { get; set; }
+    public int TabIndex { get; set; }
+    public bool IsActiveTab { get; set; }
 }
